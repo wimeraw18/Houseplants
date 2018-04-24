a36c779c810fa945a4a69c0017376d91e7a2e9e7
Alaric Wimer <wimeraw18@gmail.com>
2018-04-24 20:35:09 +0100

    Add project files

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Enter your first name.");
+var lastName = prompt("Enter your last name");
+var fullName = firstName + " " + lastName;
+alert("Welcome " + fullName + "! Thank you for visiting our site!");
